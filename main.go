9c1625edc40ac36a139b14624fa6831809504d74
Bogdan <19273017+BoRuDar@users.noreply.github.com>
2020-03-09 23:27:05 +0100

    fix

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type Request struct {
 }
 
 type Response struct {
-	Result string `json:"name"`
+	Result string `json:"result"`
 }
 
 func HandleRequest(_ context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
